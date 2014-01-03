24e2937f47989c5a141a4e7614abf3fd508268b8
emmanuel smith <manny@hp.com>
2014-01-03 15:18:43 +0000

    start converting project into client/server model.

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# this file is part of git auto which is released under the mit license.
+# go to http://opensource.org/licenses/mit for full details.
+
+# TODO(mraxilus): setup script to install client
+# TODO(mraxilus): setup script to install server
